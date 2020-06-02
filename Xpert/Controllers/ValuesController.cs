89fa8b9be12ca76fc072d79afec8537ad374b604
Andres Felipe Arias MArin <andrewsystem0322@outlook.com>
2020-06-02 11:19:01 -0500

    Finalizacion. luego de la realimentacion debo aplicar buenas practicas que conoci ayer, investigar e implementar.

---
+++ Xpert/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Xceed.Wpf.Toolkit;
+
+namespace Xpert.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            
+            
+            TextReader txt = new StreamReader("historial_de_conversaciones.txt");
+            string total = txt.ReadToEnd();
+            string delimitador = "\r\n";
+
+
+            string [] prueba = total.Split(delimitador);
+
+            return prueba;
+            //return new string[] { "value1", "value2" };
+            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
